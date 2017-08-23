9ad495edb632c8f4fd8ea7777e35ac376ef74652
jkshepperson <jkshepperson@gmail.com>
2017-08-23 13:53:01 -0500

    created the work page and got general layout done

--- strata/assets/js/main.js
+++ strata/assets/js/main.js
@@ -109,12 +109,12 @@
 
 				}
 
-		// Main Sections: Two.
+		// Main Sections: workTwo.
 
 			// Lightbox gallery.
 				$window.on('load', function() {
 
-					$('#two').poptrox({
+					$('#workTwo').poptrox({
 						caption: function($a) { return $a.next('h3').text(); },
 						overlayColor: '#2c2c2c',
 						overlayOpacity: 0.85,
